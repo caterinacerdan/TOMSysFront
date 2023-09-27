ee8d20336b02f2e061e27d4b5a7faf32603e16e4
Usuario <Usuario@DESKTOP-UPEDEMQ>
2023-09-26 21:38:12 -0300

    Agregar archivos de proyecto.

---
+++ TOMSysFront/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TOMSysFront.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
